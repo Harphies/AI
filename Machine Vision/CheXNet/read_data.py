5704e91adc0ed56a57caa3e4c9be4b66ba2d87ab
Harphies <tafeez.tech@gmail.com>
2021-01-27 13:37:22 +0100

    data loader method defined

--- Machine Vision/CheXNet/read_data.py
+++ Machine Vision/CheXNet/read_data.py
@@ -28,3 +28,25 @@ class ChestXrayDataset(Dataset):
                 image_name = os.path.join(data_dir, image_name)
                 image_names.append(image_name)
                 labels.append(label)
+
+        self.image_names = image_names
+        self.labels = labels
+        self.transform = transform
+
+    def __getimage__(self, index):
+        """
+        Args:
+            index: the index of image
+
+        Returns:
+        image and its labels
+        """
+        image_name = self.image_names[index]
+        image = Image.open(image_name).convert('RGB')
+        label = self.labels[index]
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, torch.Tensor.float(label)
+
+    def __len__(self):
+        return len(self.image_names)
