757fed2c5e76032f71e75da6f4c6d1ab301cea01
harphies <tafeez.tech@gmail.com>
2021-12-26 14:20:01 +0000

    ran some commads

--- sklearn/Regression/weather/method2.py
+++ sklearn/Regression/weather/method2.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
