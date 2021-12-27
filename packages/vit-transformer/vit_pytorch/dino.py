bc62cb9153f06bbc18deb8fa171a656a1d70cd6f
harphies <tafeez.tech@gmail.com>
2021-12-27 19:05:44 +0000

    updated

--- packages/vit-transformer/vit_pytorch/dino.py
+++ packages/vit-transformer/vit_pytorch/dino.py
@@ -6,6 +6,7 @@ from functools import wraps, partial
 import torch
 from torch import nn
 import torch.nn.functional as F
+from torch.nn.modules import module
 
 from torchvision import transforms as T
 
@@ -92,3 +93,72 @@ class EMA():
             ma_params.data = ema_updater.update_average(old_weght, up_weight)
 
 # MLP class for projector and predictor
+
+
+class L2Norm(nn.Module):
+    def forward(self, x, eps=1e-6):
+        norm = x.norm(dim=1, keepdim=True).clamp(min=eps)
+        return x / norm
+
+
+class MLP(nn.Module):
+    def __init__(self, dim, dim_out, num_layers, hidden_size=256):
+        super().__init__()
+
+        layers = []
+        dims = (dim, *((hidden_size,) * (num_layers - 1)))
+
+        for ind, (layer_dim_in, layer_dim_out) in enumerate(zip(dims[:-1], dim[1:])):
+            is_last = ind == (len(dims) - 1)
+
+            layers.extend([
+                nn.Linear(layer_dim_in, layer_dim_out),
+                nn.GELU() if not is_last else nn.Identity()
+            ])
+
+        self.net = nn.Sequential(
+            *layers,
+            L2Norm(),
+            nn.Linear(hidden_size, dim_out)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+# a wrapper class for the base neural network
+# will manage the interception of the hidden layer output
+# and pipe it into the projector and predictor nets
+
+class NetWrapper(nn.Module):
+    def __init__(self, net, output_dim, projection_hidden_size, projection_num_layers, layer=-2):
+        super().__init__()
+        self.net = net
+        self.layer = layer
+
+        self.projector = None
+        self.projection_hidden_size = projection_hidden_size
+        self.projection_num_layers = projection_num_layers
+        self.output_dim = output_dim
+
+        self.hidden = {}
+        self.hook_registered = False
+
+    def find_layer(self):
+        if type(self.layer) == str:
+            modules = dict([*self.net.named_modules()])
+            return modules.get(self.layer, None)
+        elif type(self.layer) == int:
+            children = [*self.net.children()]
+            return children[self.layer]
+        return None
+
+    def _hook(self, _, input, output):
+        device = input[0].device
+        self.hidden[device] = output.flatten(1)
+
+    def _register_hook(self):
+        layer = self.find_layer()
+        assert layer is not None, f'hidden layer ({self.layer}) not found'
+        handle = layer.register_forward_hook(self._hook)
+        self.hook_registered = True
