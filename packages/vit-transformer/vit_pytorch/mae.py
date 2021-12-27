b12357be8c3c5250e2d3ed29ec5523c0a1a102a3
harphies <tafeez.tech@gmail.com>
2021-12-27 13:22:28 +0000

    updated

--- packages/vit-transformer/vit_pytorch/mae.py
+++ packages/vit-transformer/vit_pytorch/mae.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from einops import repeat
+from torch.nn.modules.linear import Linear
 
 from vit_pytorch.vit import Transformer
 
@@ -27,3 +28,68 @@ class MAE(nn.Module):
         num_patches, encoder_dim = encoder.pos_embedding.shape[-2:]
         self.to_patch, self.patch_to_emb = encoder.to_patch_embedding[:2]
         pixel_values_per_patch = self.patch_to_emb.weight.shape[-1]
+
+        # decoder parameters
+        self.enc_to_dec = nn.Linear(
+            encoder_dim, decoder_dim) if encoder_dim != decoder_dim else nn.Identity()
+        self.mask_token = nn.Parameter(torch.randn(decoder_dim))
+        self.decoder = Transformer(dim=decoder_dim, depth=decoder_depth,
+                                   heads=decoder_heads, dim_head=decoder_dim_head, mlp_dim=decoder_dim * 4)
+        self.decoer_pos_emb = nn.Embedding(num_patches, decoder_dim)
+        self.to_pixels = nn.Linear(decoder_dim, pixel_values_per_patch)
+
+    def forward(self, img):
+        device = img.device
+
+        # get patches
+
+        patches = self.to_patch(img)
+        batch, num_patches, *_ = patches.shape
+
+        # patch to encoder tokens and add positions
+
+        tokens = self.patch_to_emb(patches)
+        tokens = tokens + self.encoder.pos_embedding[:, 1:(num_patches + 1)]
+
+        # calculate number of patches to masked, and get random indices, dividing it up for mask vs unmasked
+
+        num_masked = int(self.masking_ratio * num_patches)
+        rand_indices = torch.rand(
+            batch, num_patches, device=device).argsort(dim=-1)
+        masked_indices, unmasked_indices = rand_indices[:,
+                                                        :num_masked], rand_indices[:, num_masked:]
+
+        # get the unmasked tokens to be encoded
+
+        batch_range = torch.arange(batch, device=device[:, None])
+        tokens = tokens[batch_range, unmasked_indices]
+
+        # get the patches to be masked for the final reconstruction loss
+        masked_patches = patches[batch_range, masked_indices]
+
+        # attend width vision transformer
+        encoder_tokens = self.encoder.transformer(tokens)
+
+        # project encoder to decoder dimensions, if they are not equal - the paper says you can get away with a smaller dimension for decoder
+
+        decoder_tokens = self.enc_to_dec(encoder_tokens)
+
+        # repeat mask tokens for number of masked, and add the positions using the masked indices derived above
+
+        mask_tokens = repeat(self.mask_token, 'd -> b n d',
+                             b=batch, n=num_masked)
+        mask_tokens = mask_tokens + self.decoder_pos_emb(masked_indices)
+
+        # concat the masked tokens to the decoder tokens and attend with decoder
+
+        decoder_tokens = torch.cat((mask_tokens, decoder_tokens), dim=1)
+        decoder_tokens = self.decoder(decoder_tokens)
+
+        # splice out the mask token and project to pixel values
+
+        mask_tokens = decoder_tokens[:, :num_masked]
+        pre_pixel_values = self.to_pixels(mask_tokens)
+
+        # calculate reconstruction loss
+        recon_loss = F.mse_loss(pre_pixel_values, masked_patches)
+        return recon_loss
