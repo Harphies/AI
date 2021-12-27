449d4573d3bc405b1ee4f8c56c9db1a2e944444a
harphies <tafeez.tech@gmail.com>
2021-12-27 01:28:22 +0000

    updated

---
+++ packages/vit-transformer/vit_pytorch/mae.py
@@ -0,0 +1,29 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from einops import repeat
+
+from vit_pytorch.vit import Transformer
+
+
+class MAE(nn.Module):
+    def __init__(
+        self,
+        *,
+        encoder,
+        decoder_dim,
+        masking_ratio=0.75,
+        decoder_depth=1,
+        decoder_heads=8,
+        decoder_dim_head=64
+    ):
+        super().__init__()
+        assert masking_ratio > 0 and masking_ratio < 1, 'masking ratio must be between 0 and 1'
+        self.masking_ratio = masking_ratio
+
+        # extract some hyperparameters and functions from encoder (vision transformer to be trained)
+
+        self.encoder = encoder
+        num_patches, encoder_dim = encoder.pos_embedding.shape[-2:]
+        self.to_patch, self.patch_to_emb = encoder.to_patch_embedding[:2]
+        pixel_values_per_patch = self.patch_to_emb.weight.shape[-1]
